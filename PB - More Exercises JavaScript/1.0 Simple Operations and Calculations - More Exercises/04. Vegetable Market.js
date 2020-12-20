c0439a31a9749bd8526d869e37746b18f0e8fec5
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-12-20 22:30:30 +0200

    Delete 04. Vegetable Market.js

--- PB - More Exercises JavaScript/1.0 Simple Operations and Calculations - More Exercises/04. Vegetable Market.js
+++
@@ -1,9 +0,0 @@
-function vegetableMarket(...arg) {
-    let [vegetable, fruit, kgVegetable, kgFruit] = [...arg.map(Number)]
-    return ((vegetable * kgVegetable + fruit * kgFruit) / 1.94).toFixed(2)
-}
-
-// console.log(vegetableMarket(0.194, 19.4, 10, 10))  //101.00
-
-
-
