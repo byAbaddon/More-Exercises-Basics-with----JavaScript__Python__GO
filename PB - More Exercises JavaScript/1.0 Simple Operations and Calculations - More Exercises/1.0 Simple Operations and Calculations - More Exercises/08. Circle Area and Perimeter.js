5d62352569fc86873e14ab90a6c12d3bc8c8e02d
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-12-21 02:38:09 +0200

    Delete 08. Circle Area and Perimeter.js

--- PB - More Exercises JavaScript/1.0 Simple Operations and Calculations - More Exercises/1.0 Simple Operations and Calculations - More Exercises/08. Circle Area and Perimeter.js
+++
@@ -1,7 +0,0 @@
-function circleAreaAndPerimeter(n) {
-    let face = Math.PI * n * n
-    let prm =  Math.PI * 2 * n
-    return `${face.toFixed(2)}\n${prm.toFixed(2)}`  
-}
-
-// console.log(circleAreaAndPerimeter(3)) //28.27   / 18.85
