83ebe3b6ba42ed5e80821ab5a9380977940bb863
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-02-08 01:15:46 +0200

    Update 05. Training Lab.js

--- PB - More Exercises JavaScript/1.0 Simple Operations and Calculations - More Exercises/05. Training Lab.js
+++ PB - More Exercises JavaScript/1.0 Simple Operations and Calculations - More Exercises/05. Training Lab.js
@@ -1,3 +1,6 @@
+([w, h]) => (w * 100 / 120 | 0) * (((h * 100) - 100) / 70 | 0) - 3
+
+//----------------------------------------------------------------------------or (2)
 function trainingLab([width, len]) {
   let rows = Math.trunc(width * 100 / 120)
   let desk = Math.trunc(((len * 100) - 100) / 70)
@@ -5,31 +8,3 @@ function trainingLab([width, len]) {
   return allPlace
 }
 //console.log(trainingLab([15, 8.9]))
-
-//----------------------------------------------------------------------------or (2)
-
-
-
-function trainingLab(...arg) {
-    let [height, width] = [...arg.map(Number)]
-    let hallway = 100
-    let lostWorkPosition = 3
-    let workPositionWidth = 70
-    let workPositionHeight = 120
-
-    height *= 100
-    width *= 100
-    width -= hallway
-
-    let rest = width % workPositionWidth
-    width -= rest
-    let bureauCount = width / workPositionWidth
-
-    rest = height % workPositionHeight
-    height -= rest
-    let rowCount = height / workPositionHeight
-
-    return bureauCount * rowCount - lostWorkPosition
-}
-
-// console.log(trainingLab(15, 8.9))  //129
