ba54dfee796152fc2108490529ed3e3e7ac09f8c
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-03-17 16:40:55 +0200

    Update 03. Flowers.py

--- PB - More Exercises Python/3.0 Nested Conditional Statements - More Exercises/03. Flowers.py
+++ PB - More Exercises Python/3.0 Nested Conditional Statements - More Exercises/03. Flowers.py
@@ -1,3 +1,22 @@
+c, r, t, season, holiday = [input() if i > 2 else int(input()) for i in range(5)]
+data_dict = {'s_s': {'c': c * 2, 'r': 4.1 * r, 't': 2.5 * t}, 'a_w': {'c': c * 3.75, 'r': 4.5 * r, 't': 4.15 * t} }
+
+if season == 'Spring' or season == 'Summer':
+    price = sum(data_dict['s_s'].values())
+else:
+    price = sum(data_dict['a_w'].values())
+
+if holiday == 'Y': price *= 1.15
+if season == 'Spring' and t > 7: price *= 0.95
+if season == 'Winter' and r >= 10: price *= 0.90
+if c + r + t > 20: price *= 0.8
+
+print(f'{(price + 2):.2f}')
+
+
+#--------------------------------------------------------------------(2)--------------------------------------
+
+
 chrysanthemums , roses, tulips = [int(input()) for _ in range(3)]
 season, is_celebrate_day = [input() for _ in range(2)]
 
