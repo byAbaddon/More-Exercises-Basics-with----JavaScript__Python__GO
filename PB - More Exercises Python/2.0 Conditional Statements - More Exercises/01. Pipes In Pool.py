756a6ea9f5a4dd57ee31ac901e87f772381632f7
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-04 00:39:36 +0300

    Update 01. Pipes In Pool.py

--- PB - More Exercises Python/2.0 Conditional Statements - More Exercises/01. Pipes In Pool.py
+++ PB - More Exercises Python/2.0 Conditional Statements - More Exercises/01. Pipes In Pool.py
@@ -2,9 +2,9 @@ volume, pipe_one, pipe_two, hours = [float(input()) for _ in range(4)]
 pipe_one *= hours
 pipe_two *= hours
 pool = pipe_one + pipe_two
-pool_percent = (pool /volume) * 100
-pipe_one_percent = (pipe_one / pool) * 100
-pipe_two_percent = (pipe_two / pool) * 100
+pool_percent = pool / volume * 100
+pipe_one_percent = pipe_one / pool * 100
+pipe_two_percent = pipe_two / pool * 100
 
 if pool <= volume:
     print(f'The pool is {pool_percent:.2f}% full. Pipe 1: {pipe_one_percent:.2f}%. Pipe 2: {pipe_two_percent:.2f}%.')
