4f21887dd73452a635fd5f560bc744d411be44e0
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-05 16:04:18 +0300

    Update 03. Harvest.js

--- PB - More Exercises JavaScript/2.0 Conditional Statements - More Exercises/03. Harvest.js
+++ PB - More Exercises JavaScript/2.0 Conditional Statements - More Exercises/03. Harvest.js
@@ -1,3 +1,16 @@
+function harvest([vineyard, kvM, necessaryWine, workers]) {
+    let allWine = vineyard * 0.40 * kvM / 2.5
+    let bonusWine = allWine - necessaryWine
+    let wineForWorkers = bonusWine / workers
+ 
+    if (necessaryWine > allWine)
+        return `It will be a tough winter! More ${Math.abs(Math.trunc(bonusWine))} liters wine needed.`
+    return `Good harvest this year! Total wine: ${~~allWine} liters.\n${Math.ceil(bonusWine)} liters left -> ${Math.ceil(wineForWorkers)} liters per person.`
+}
+
+//-----------------------------------------------(2)  //old input no't array
+
+
 function harvest(...arg) {
     let [vineyard, kvM, necessaryWine, workers] = [...arg.map(Number)]
     let allWine = (vineyard *= 0.40) * kvM / 2.5
