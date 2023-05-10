a038a440ef546e43248f0ed25d8175e643c9f120
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-10 03:44:48 +0300

    Update 08. Fuel Tank.py

--- PB - More Exercises Python/2.0 Conditional Statements - More Exercises/08. Fuel Tank.py
+++ PB - More Exercises Python/2.0 Conditional Statements - More Exercises/08. Fuel Tank.py
@@ -1,3 +1,12 @@
+type_fuel, quantity = input().lower(), int(input())
+
+if type_fuel not in ['diesel','gasoline','gas']: print('Invalid fuel!')
+elif quantity >= 25: print(f'You have enough {type_fuel}.')
+else: print(f'Fill your tank with {type_fuel}!')
+
+#-------------------------------------------(2)------------------------------------------
+    
+
 type_fuel, quantity = input(), int(input())
 lst = ['Diesel','Gasoline','Gas']
 
