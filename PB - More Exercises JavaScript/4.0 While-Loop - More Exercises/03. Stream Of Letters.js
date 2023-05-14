b88ade532b21db68ca8ea984608f33cf88477d8e
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-14 22:43:19 +0300

    Update 03. Stream Of Letters.js

--- PB - More Exercises JavaScript/4.0 While-Loop - More Exercises/03. Stream Of Letters.js
+++ PB - More Exercises JavaScript/4.0 While-Loop - More Exercises/03. Stream Of Letters.js
@@ -1,34 +1,28 @@
 function streamOfLetters(arg) {
-   let wordCollection = ''
-   let word = ''
-   let [c, o, n] = [false, false, false]
+  let wordCollection = word = ''
+  let [c, o, n] = [false, false, false]
 
-   for (let i = 0; i < arg.length; i++) {
-      let chars = arg[i]
-      
-      if (/[^A-Za-z]/.test(chars)) {
-         continue
-      }
+  for (let i = 0; i < arg.length; i++) {
+     let char = arg[i]
+     
+     if (/[^A-Za-z]/.test(char)) continue
+  
+     if (char == 'c' && !c || char == 'o' && !o || char == 'n' && !n) {
+        if (char == 'c') c = true
+        if (char == 'o') o = true
+        if (char == 'n') n = true
+     } else {
+        word += char
+     }
 
-      if ((chars == 'c' && c != true) || (chars == 'o' && o != true) || (chars == 'n' && n != true)) {
+     if (c && o && n) {
+        wordCollection += word + ' '
+        c = o = n = false
+        word = ''
+     }
+  }
 
-         if (chars === 'c') c = true
-         if (chars === 'o') o = true
-         if (chars === 'n') n = true
-      } else {
-         word += chars
-      }
-
-      if (c && o && n) {
-         wordCollection += word + ' '
-         c = false
-         o = false
-         n = false
-         word = ''
-      }
-   }
-
-   return wordCollection
+  return wordCollection
 }
 
 //console.log((streamOfLetters(['H', 'n', 'e', 'l', 'l', 'o', 'o', 'c', 't', 'c', 'h', 'o', 'e', 'r', 'e', 'n', 'e', 'End'])))
