a9512bf9e65d74deb19731a22fa3a5b4ac099db6
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-04 20:41:13 +0300

    Update 05. Game Of Intervals.js

--- PB - More Exercises JavaScript/5.0 For-Loop - More Exercises/05. Game Of Intervals.js
+++ PB - More Exercises JavaScript/5.0 For-Loop - More Exercises/05. Game Of Intervals.js
@@ -1,4 +1,39 @@
 function gameOfIntervals(arg) {
+  const percentObj = {'From 0 to 9:': 0, 'From 10 to 19:': 0, 'From 20 to 29:': 0, 'From 30 to 39:': 0, 'From 40 to 50:': 0, 'Invalid numbers:': 0}
+  let points = 0
+
+  arg.slice(1).forEach(currentNum => {
+    if (0 <= currentNum && currentNum <= 9) {
+      points += currentNum * 0.20
+      percentObj['From 0 to 9:']++
+    } else if (10 <= currentNum && currentNum <= 19) {
+      points += currentNum * 0.30
+      percentObj['From 10 to 19:']++
+    } else if (20 <= currentNum && currentNum <= 29) {
+      points += currentNum * 0.40
+      percentObj['From 20 to 29:']++
+    } else if (30 <= currentNum && currentNum <= 39) {
+      points += 50
+      percentObj['From 30 to 39:']++
+    } else if (40 <= currentNum && currentNum <= 50) {
+      points += 100
+      percentObj['From 40 to 50:']++
+    } else {
+      points /= 2
+      percentObj['Invalid numbers:']++
+    }
+  })
+
+  console.log(points.toFixed(2))
+  for (const [key, val] of Object.entries(percentObj)) {
+    console.log(`${key} ${(val / arg[0] * 100).toFixed(2)}%`)
+  }
+}
+
+
+//---------------------------------------------------------(2)-----------------------------------
+
+function gameOfIntervals(arg) {
     arg = [...arg.map(Number)]
     let move = arg.shift()
     let points = 0
@@ -33,5 +68,6 @@ function gameOfIntervals(arg) {
         console.log(`${key} ${(val / move * 100).toFixed(2)}%`)
     }
 }
+
 
 // gameOfIntervals([10, 43, 57, -12, 23, 12, 0, 50, 40, 30, 20])
