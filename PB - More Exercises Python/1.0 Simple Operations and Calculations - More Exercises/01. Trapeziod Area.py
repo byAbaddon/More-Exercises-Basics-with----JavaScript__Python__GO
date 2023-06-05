f14ab9cb3baf2b0bcc090259164fd48b834beea4
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-05 17:53:14 +0300

    Update 01. Trapeziod Area.py

--- PB - More Exercises Python/1.0 Simple Operations and Calculations - More Exercises/01. Trapeziod Area.py
+++ PB - More Exercises Python/1.0 Simple Operations and Calculations - More Exercises/01. Trapeziod Area.py
@@ -1,6 +1,14 @@
 b1, b2, h, = [float(input()) for _ in range(3)]
 print(f'{(b1 + b2) * h / 2:.2f}')
 
+#----------------------------(2)----------------------------
+
+b1 = int(input())
+b2 = int(input())
+h = int(input())
+area = (b1 + b2) * h / 2
+print(f'{area:.2f}')
+
 
 '''
 8
