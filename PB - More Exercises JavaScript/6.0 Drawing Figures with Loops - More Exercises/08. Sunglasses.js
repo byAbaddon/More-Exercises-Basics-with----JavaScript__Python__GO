bcf349458d43a61aef674a2a5b371356b2ebee23
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-07-12 15:15:38 +0300

    Update 08. Sunglasses.js

--- PB - More Exercises JavaScript/6.0 Drawing Figures with Loops - More Exercises/08. Sunglasses.js
+++ PB - More Exercises JavaScript/6.0 Drawing Figures with Loops - More Exercises/08. Sunglasses.js
@@ -2,16 +2,11 @@ function sunglasses(n) {
     n = +n
     //top
     console.log(`${'*'.repeat(2 * n)}${' '.repeat(n) }${'*'.repeat(2 * n)}`)
-
     //middle
     for (i = 0; i < n - 2; i++) {
-        if (i === ~~((n - 1) / 2 - 1)) {
-            console.log('*' + '/'.repeat(n + (n - 2)) + '*' + '|'.repeat(n) + '*' + '/'.repeat(n + (n - 2)) + '*')
-        } else {
-            console.log('*' + '/'.repeat(n + (n - 2)) + '*' + ' '.repeat(n) + '*' + '/'.repeat(n + (n - 2)) + '*')
-        }
+      i == ~~((n - 1) / 2 - 1) ? s = '|' : s = ' '
+      console.log('*' + '/'.repeat(n + (n - 2)) + '*' + s.repeat(n) + '*' + '/'.repeat(n + (n - 2)) + '*')
     }
-
     //bottom
     console.log(`${'*'.repeat(2 * n)}${' '.repeat(n) }${'*'.repeat(2 * n)}`)
 }
